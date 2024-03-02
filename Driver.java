3577dd2b22b13f8ca3027013efc5b28a5c16b6ff
Avery Lee <124991479+tunafishing@users.noreply.github.com>
2024-03-02 00:35:17 -0500

    Driver code

---
+++ Driver.java
@@ -0,0 +1,20 @@
+/**
+ * The Driver class is responsible for starting the program.
+ *
+ * UOttaHack 6
+ *
+ * @version 1.0
+ * @author Avery Lee
+ *         Created on 2024/03/03
+ */
+public class Driver {
+    /**
+     * The main method that starts the program by creating an instance of the
+     * Organizer class.
+     * 
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Organizer(); // start the program
+    }
+}
